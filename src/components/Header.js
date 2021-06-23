649f4d6db8722121d89cb9cfea5d56e37c2f0489
Sara-ELMustafah <saraelmustafah@gmail.com>
2021-06-23 16:43:33 +0300

    my first react app done

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+       <h1>Gallery of Hornes</h1>
+        )
+    }
+}
+
+export default Header;
