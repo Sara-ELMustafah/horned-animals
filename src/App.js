aec38265eacc7ae2e84ecf59ad5ecf64b5bd4804
Sara-ELMustafah <saraelmustafah@gmail.com>
2021-06-24 03:05:19 +0300

    Changes

--- src/App.js
+++ src/App.js
@@ -4,27 +4,6 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends React.Component{
   render(){
     return(
@@ -37,3 +16,6 @@ class App extends React.Component{
   }
 }
 export default App;
+
+
+// You can use an empty tag <></> instead of the <div></div> 
