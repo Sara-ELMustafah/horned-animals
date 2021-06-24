19d7f0753dcc608b6ffa09d2607a5c31d3f71cee
Sara-ELMustafah <saraelmustafah@gmail.com>
2021-06-25 02:17:00 +0300

    Done with lab02

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
+import HornsData from './HornsData.json';
 
 
-class Main extends React.Component{
-    render(){
-        return(
-        <div>
-            {/* Sending the props to HornedBeasts */}
+class Main extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                {/* Sending the props to HornedBeasts */}
 
-            <HornedBeasts
+                {HornsData.map(elemnet => {
+                    return(
+                        <HornedBeasts //This's the call back function
+                        title={elemnet.title}
+                        imageUrl={elemnet.image_url}
+                        description={elemnet.description}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+export default Main;
+
+{/* <HornedBeasts
              title={'UniWhal'}
              imageUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
              description={'A unicorn and a narwhal nuzzling their horns'}
@@ -18,11 +36,4 @@ class Main extends React.Component{
              title='Rhino Family'
              imageUrl='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'
              description='Mother (or father) rhino with two babies'
-            />  
-             
-        </div>
-        )
-    }
-}
-
-export default Main;
+            /> */}
